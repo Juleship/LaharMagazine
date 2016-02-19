cf992813bd4253f56417156562d27fa483fe9f53
Francesco Serra <afnecors@gmail.com>
2016-02-19 11:08:29 +0100

    Change color of sidebar (edition color + opacity)

--- js/build/production.js
+++ js/build/production.js
@@ -10297,13 +10297,30 @@ $( document ).ready(function() {
   }
   
   // change color of edition
+  function hex2rgba(c, o){
+    var patt = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
+    var matches = patt.exec(c);
+    var rgba = "rgba("+parseInt(matches[1], 16)+","+parseInt(matches[2], 16)+","+parseInt(matches[3], 16)+","+ o +")";
+    return rgba;
+  }
+
   var color_code = $("#edition_color").val();
+  var color_opacity = "0.2";
+  rgba = hex2rgba(color_code, color_opacity);
+
   $("hr").each(function(){
     $(this).css("border-color", color_code);
   });
   $("body").css("border-color", color_code);
   $("#video-opacity").css("background-color", color_code).css("opacity", "0.3");
+  $(".rubrica").each(function(){
+    $(this).css("background", rgba);
+  });
+
+
+
 });
+
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
